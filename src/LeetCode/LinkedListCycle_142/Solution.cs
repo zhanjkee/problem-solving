397ff94a2d76b4459a8f26b750a7a19acc5f4bbb
Zhangeldy Kalkenov <Zhangeldy.Kalkenov@airastana.com>
2022-10-12 10:35:02 +0600

    [add] detect cycle for singly linked list

---
+++ src/LeetCode/LinkedListCycle_142/Solution.cs
@@ -0,0 +1,82 @@
+using Xunit;
+
+namespace LinkedListCycle_142;
+
+public class ListNode
+{
+	public ListNode(int value)
+	{
+		Value = value;
+	}
+
+	public int Value { get; set; }
+	public ListNode? Next { get; set; }
+}
+
+public class Solution
+{
+	[Fact]
+	public void Check()
+	{
+		// Arrange.
+		var head = GetCycledListNode(out var excepted);
+
+		// Act.
+		var result = DetectCycle(head);
+
+		// Assert.
+		Assert.Equal(excepted, result);
+	}
+
+	public ListNode? DetectCycle(ListNode? head)
+	{
+		if (head == null)
+			return null;
+
+		var intersect = GetIntersect(head);
+
+		if (intersect == null)
+			return null;
+
+		var firstPointer = head;
+		var secondPointer = intersect;
+
+		while (firstPointer != secondPointer)
+		{
+			firstPointer = firstPointer?.Next;
+			secondPointer = secondPointer?.Next;
+		}
+
+		return firstPointer;
+	}
+
+	private ListNode? GetIntersect(ListNode? head)
+	{
+		var fast = head;
+		var slow = head;
+
+		while (fast != null && fast.Next != null)
+		{
+			fast = fast.Next.Next;
+			slow = slow?.Next;
+
+			if (slow == fast) return slow;
+		}
+
+		return null;
+	}
+
+	private ListNode GetCycledListNode(out ListNode excepted)
+	{
+		var head = new ListNode(1);
+		excepted = new ListNode(2);
+		var third = new ListNode(3);
+		var fourth = new ListNode(4);
+		head.Next = excepted;
+		excepted.Next = third;
+		third.Next = fourth;
+		fourth.Next = excepted;
+
+		return head;
+	}
+}
